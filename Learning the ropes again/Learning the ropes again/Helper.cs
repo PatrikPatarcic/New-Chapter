b1068ee33713f4f078075283b9fa097f41aed8b5
PatrikPatarcic <patrikizopatovca@gmail.com>
2025-10-26 12:04:50 +0100

    Helper class for easier color printing

---
+++ Learning the ropes again/Learning the ropes again/Helper.cs
@@ -0,0 +1,14 @@
+﻿public static class Helper
+{
+    public static void Print<T>(T message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public static void Print<T>(T message)
+    {
+        Console.WriteLine(message);
+    }
+}
