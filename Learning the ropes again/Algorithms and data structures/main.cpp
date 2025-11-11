50636bbf0aa54537935294bd8d5e8fe8e3b7aed0
PatrikPatarcic <patrikizopatovca@gmail.com>
2025-11-12 00:46:12 +0100

    Rubbish calendar

--- Learning the ropes again/Algorithms and data structures/main.cpp
+++ Learning the ropes again/Algorithms and data structures/main.cpp
@@ -26,7 +26,33 @@
 //	if(n == 0) return 0;
 //	return (n / 10) + zbrojZnam(n % 10);
 //}
-//
+    int matrice[3][3] = {	{1, 2, 6},
+							{3, 4, 7},
+							{5, 8, 9} };
+
+	int arr[9] = {};
+
+	// 1 3 5  / 2 4 8  / 6 7 9
+
+	// 1 2 3 5 8 / 6 7 9
+
+
+	// Print sorted array, but the array's columns are
+	// sorted in ascending order.
+	// matrice 3x3 is an example, but it should work
+	// with any NxN matrice.
+	void sortedArr(int matrice[3][3]) {
+		int helperArr1[3];
+		int helperArr2[3];
+		int helperArr3[3];
+
+		for (int i = 0; i < 3; i++) {
+			helperArr1[i] = matrice[i][0];
+			helperArr2[i] = matrice[i][1];
+			helperArr3[i] = matrice[i][3];
+		}
+	}
+
 int main() {
 	//std::cout << "Hello Worlds" << std::endl;
 
@@ -34,7 +60,7 @@ int main() {
 	//rek(128);
 	//std::cout << zbrojZnam(234) << std::endl;
 	
-	listDemo();
+	/*listDemo();
 	listDemo();
 	listDemo();
 
@@ -43,6 +69,8 @@ int main() {
 	head = AddNode(head, 54);
 	head = AddNode(head, 932);
 	PrintList(head);
-	
+	*/
+
+
 	return 0;
 }
---
+++ Learning the ropes again/Proper Calender/Program.cs
@@ -0,0 +1,263 @@
+﻿using System.Net.Http.Headers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProperCalender;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine(DateRighter.GetProperDate(DateTime.Now));
+        //Console.WriteLine(DateRighter.GetProperDate(29, 2, 2025));
+        //Console.WriteLine(DateRighter.GetProperDate(28, 2, 2025));
+        //Console.WriteLine(DateRighter.GetProperDate(27, 2, 2025));
+        //Console.WriteLine(DateRighter.GetProperDate(30, 2, 2025));
+        //Console.WriteLine(DateRighter.GetProperDate(31, 3, 2025));
+        //Console.WriteLine(DateRighter.GetProperDate(11, 11, 2025));
+        Console.WriteLine(DateRighter.GetProperDate(31, 12, 2025));
+        Console.WriteLine(DateRighter.GetProperDate(31, 12, 2020));
+        //Console.WriteLine(DateRighter.GetProperDate(30, 12, 2025));
+        //Console.WriteLine(DateRighter.GetProperDate(25, 12, 2025));
+        Console.WriteLine(DateRighter.GetProperDate(27, 2, 2020));
+        Console.WriteLine(DateRighter.GetProperDate(28, 2, 2020));
+        Console.WriteLine(DateRighter.GetProperDate(29, 2, 2020));
+        Console.WriteLine(DateRighter.GetProperDate(30, 2, 2020));
+        Console.WriteLine(DateRighter.GetProperDate(1, 3, 2020));
+    }
+}
+
+class DateRighter
+{
+    private static void TurnTheMonth(ref int day, ref int month)
+    {
+        month++;
+        day = day - 28;
+    }
+
+    public static string GetProperDate(DateTime date)
+    {
+        int day = date.Day;
+        int month = date.Month;
+        int daysInMonth = DateTime.DaysInMonth(date.Year, month);
+
+        int addForLeapYear = 0;
+        if (DateTime.IsLeapYear(date.Year)) addForLeapYear = 1;
+
+        switch (month)
+        {
+            case 1:
+                if (day > 28)
+                {
+                    TurnTheMonth(ref day, ref month);
+                }
+                break;
+            case 2:
+                day = day + 3;
+                if (day > 25)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 3:
+                day = day + 3 + addForLeapYear;
+                if (day > 22)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 4:
+                day = day + 6 + addForLeapYear;
+                if (day > 20)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 5:
+                day = day + 11 + addForLeapYear;
+                if (day > 17)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 6:
+                day = day + 13 + addForLeapYear;
+                if (day > 15)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 7:
+                day = day + 16 + addForLeapYear;
+                if (day > 12)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 8:
+                day = day + 19 + addForLeapYear;
+                if (day > 9)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 9:
+                day = day + 21 + addForLeapYear;
+                if (day > 6)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 10:
+                day = day + 24 + addForLeapYear;
+                if (day > 4)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 11:
+                day = day + 26 + addForLeapYear;
+                if (day > 2)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 12:
+                day = day + 26 + addForLeapYear;
+                if (day > 2)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            default:
+                break;
+        }
+        return $"{day}.{month}.{date.Year}";
+    }
+    public static string GetProperDate(int day, int month, int year)
+    {
+        day = Math.Clamp(day, 1, DateTime.DaysInMonth(year, month));
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+        int addForLeapYear = 0;
+        if (DateTime.IsLeapYear(year)) addForLeapYear = 1;
+
+        switch (month)
+        {
+            case 1:
+                if (day > 28)
+                {
+                    TurnTheMonth(ref day, ref month);
+                }
+                break;
+            case 2:
+                day = day + 3;
+                if (day > 25)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 3:
+                day = day + 3 + addForLeapYear;
+                if (day > 22)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 4:
+                day = day + 6 + addForLeapYear;
+                if (day > 20)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 5:
+                day = day + 11 + addForLeapYear;
+                if (day > 17)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 6:
+                day = day + 13 + addForLeapYear;
+                if (day > 15)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 7:
+                day = day + 16 + addForLeapYear;
+                if (day > 12)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 8:
+                day = day + 19 + addForLeapYear;
+                if (day > 9)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 9:
+                day = day + 21 + addForLeapYear;
+                if (day > 6)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 10:
+                day = day + 24 + addForLeapYear;
+                if (day > 4)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 11:
+                day = day + 26 + addForLeapYear;
+                if (day > 2)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            case 12:
+                day = day + 26 + addForLeapYear;
+                if (day > 2)
+                {
+                    TurnTheMonth(ref day, ref month);
+
+                }
+                break;
+            default:
+                break;
+        }
+        return $"{day}.{month}.{year}";
+    }
+}
+
+
+static class DateTimeExpensions
+{
+    static string ProperDate(this DateTime dateTime, DateTime date)
+    {
+        return DateRighter.GetProperDate(date);
+    }
+}
